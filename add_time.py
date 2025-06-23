60a6798d70e6b442d9da6a3f33171025cdd5b958
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-06-23 18:34:17 +0300

    Make sudoku solver

--- add_time.py
+++ add_time.py
@@ -44,9 +44,4 @@ def add_time(start, duration, day_of_week=None):
         new_time += f" ({days_later} days later)"
 
     return new_time
-print(add_time("3:00 PM", "3:10"))                      # 6:10 PM
-print(add_time("11:30 AM", "2:32", "Monday"))           # 2:02 PM, Monday
-print(add_time("11:43 AM", "00:20"))                    # 12:03 PM
-print(add_time("10:10 PM", "3:30"))                     # 1:40 AM (next day)
-print(add_time("11:43 PM", "24:20", "tueSday"))         # 12:03 AM, Thursday (2 days later)
-print(add_time("6:30 PM", "205:12"))                    # 7:42 AM (9 days later)
+print(add_time("3:00 PM", "3:10"))                      
